c8ba7993505f8438dc04651984816923214af2ff
Leonard Shen <info@uvstudio.ca>
2019-08-11 18:10:47 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,224 @@
+$(document).ready(function(){
+
+  $('.active-icon').css('opacity', 1);
+
+//home page
+
+  $('#piers-morgan-icon-home').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#piers-home-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#piers-home-video').addClass('block');
+      $('#piers-home-video').removeClass('none');
+      $('#piers-morgan-icon-home-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#when-calls-icon-home').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#when-calls-home-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#when-calls-home-video').addClass('block');
+      $('#when-calls-home-video').removeClass('none');
+      $('#when-calls-icon-home-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#cypress-hill-icon-home').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#cypress-home-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#cypress-home-video').addClass('block');
+      $('#cypress-home-video').removeClass('none');
+      $('#cypress-hill-icon-home-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#unforgotten-icon-home').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#unforgotten-home-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#unforgotten-home-video').addClass('block');
+      $('#unforgotten-home-video').removeClass('none');
+      $('#unforgotten-icon-home-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#unforgotten-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#unforgotten-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#unforgotten-video').addClass('block');
+      $('#unforgotten-video').removeClass('none');
+      $('#unforgotten-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#billion-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#billion-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#billion-video').addClass('block');
+      $('#billion-video').removeClass('none');
+      $('#billion-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#when-calls-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#when-calls-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#when-calls-video').addClass('block');
+      $('#when-calls-video').removeClass('none');
+      $('#when-calls-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#costumes-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#costumes-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#costumes-video').addClass('block');
+      $('#costumes-video').removeClass('none');
+      $('#costumes-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#virginian-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#virginian-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#virginian-video').addClass('block');
+      $('#virginian-video').removeClass('none');
+      $('#virignian-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#cypress-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#cypress-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#cypress-video').addClass('block');
+      $('#cypress-video').removeClass('none');
+      $('#cypress-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#guerilla-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#guerrilla-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#guerrilla-video').addClass('block');
+      $('#guerrilla-video').removeClass('none');
+      $('#guerilla-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#heavy-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#heavy-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#heavy-video').addClass('block');
+      $('#heavy-video').removeClass('none');
+      $('#heavy-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+  
+  $('#dying-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#dying-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#dying-video').addClass('block');
+      $('#dying-video').removeClass('none');
+      $('#dying-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('#piers-morgan-icon').click(function(){
+    var currentVideo = $('.block');
+    var currentIcon = $('.active-icon');
+    currentVideo.fadeOut(150, function(){
+      $('#piers-video').fadeIn(150);
+      currentVideo.addClass('none');
+      currentVideo.removeClass('block');
+      currentIcon.removeClass('active-icon').css('opacity', '');
+      $('#piers-video').addClass('block');
+      $('#piers-video').removeClass('none');
+      $('#piers-morgan-icon-img').addClass('active-icon').css('opacity', 1);
+    });
+  });
+
+  $('.mail-icon').click(function(){
+    if($('.email-options').hasClass('none-email')){
+      $('.email-options').addClass('block-email');
+      $('.email-options').removeClass('none-email');
+    } else if($('.email-options').hasClass('block-email')){
+      $('.email-options').addClass('none-email');
+      $('.email-options').removeClass('block-email');
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
